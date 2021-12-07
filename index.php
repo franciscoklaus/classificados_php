08a8a198ea9193b37dbce263e7b182d51ceb3377
josé Francisco <franciscoklausfilho@hotmail.com>
2021-12-07 17:30:15 -0300

    criacao da pasta pages onde criei header e footer

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <title>Classificados</title>
-</head>
-<body>
-    <nav class="navbar navbar-inverse" >
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a href="./" class='navbar-brand'>Classificados</a>
-            </div>
-            <ul class='nav navbar-nav navbar-right'>
-                <li><a href="">Cadastre-se</a></li>
-                <li><a href="">Login</a></li>
-                <!-- SE ESTIVEREM LOGADOS DEVE APARECERO O QUE TEM ABAIXO-->
-                <li><a href="">Meus anúncios</a></li>
-                <li><a href="">Sair</a></li>
-            </ul>
-        </div>
-    </nav>
+<?php require 'pages/header.php'; ?>
+
     <div class="container-fluid">
         <div class="jumbotron">
             <h3>Nos temos hoje 999 anúncios.</h3>
@@ -43,9 +19,4 @@
         </div>
     </div>
 
-
-
-    <script rel="stylesheet" href="assets/js/scripts.js">   </script> 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php require "pages/footer.php"; ?>
---
+++ pages/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Classificados</title>
+</head>
+<body>
+    <nav class="navbar navbar-inverse" >
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="./" class='navbar-brand'>Classificados</a>
+            </div>
+            <ul class='nav navbar-nav navbar-right'>
+                <li><a href="">Cadastre-se</a></li>
+                <li><a href="">Login</a></li>
+                <!-- SE ESTIVEREM LOGADOS DEVE APARECERO O QUE TEM ABAIXO-->
+                <li><a href="">Meus anúncios</a></li>
+                <li><a href="">Sair</a></li>
+            </ul>
+        </div>
+    </nav>
