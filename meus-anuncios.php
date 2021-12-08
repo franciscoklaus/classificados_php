a05db7c575054882e9e2fecf29e89aa8f1f4c3aa
josé Francisco <franciscoklausfilho@hotmail.com>
2021-12-07 21:19:47 -0300

    criacao da página meus-anuncios e sua classe

--- meus-anuncios.php
+++ meus-anuncios.php
@@ -1,10 +1,18 @@
+<?php require "pages/header.php";?>
+<?php 
+    if(empty($_SESSION['cLogin'])){ 
+?>
+<script type='text/javascript'>
+    window.location.href='./';
+</script>
 <?php
-
-    require "pages/header.php";
+    exit;
+    }
 ?>
 <div class="container">
 
     <h1>Meus anúncios</h1>
+    <a href="add-anuncio.php" class='btn btn-default'>Adicionar anúncio</a>
     <table class="table table-striped">
         <thead>
             <tr>
@@ -14,13 +22,21 @@
                 <th>Açoes</th>
             </tr>
         </thead>
-        <?php
-            
-        ?>
         <tbody>
-            <tr>
-                <td></td>
-            </tr>
+        <?php
+           require 'classes/anuncios.class.php';
+           $a = new Anuncios();
+           $anuncios = $a->getMeusAnuncios();
+           foreach($anuncios as $anuncio):
+        ?>
+        <tr>
+            <td><img src="assets/images/anuncios/<?php $anuncio['url']; ?>" border="0" alt='foto'></td>
+            <td><?php echo $anuncio['titulo'];?></td>
+            <td>R$ <?php echo number_format($anuncio['valor'],2);?></td>
+            <td>Açoes</td>
+
+        </tr>
+        <?php endforeach; ?>
         </tbody>
     </table>
 </div>
