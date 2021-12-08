713e1e0f3afa3bba32b562782dbeb2742628481d
josé Francisco <franciscoklausfilho@hotmail.com>
2021-12-07 22:24:39 -0300

    configuracao do banco online

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     session_start();
     global $pdo;
     try {
-        $pdo = new PDO("mysql:dbname=classificados;host=127.0.0.1","root",'');
+        $pdo = new PDO("mysql:dbname=teste_php_mysql;host=85.10.205.173:3306","franciscoklaus","@Ez'EB3K*wBz4h@");
     } catch (PDOException $e) {
         echo "FALHOU! " .$e->getMessage();
         exit;
